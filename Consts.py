ac0d2bc2718e15cddbcd6af7ba9625aa95478437
pink-potao <lihi.zachis1223@gmail.com>
2022-09-21 11:11:18 +0300

    screen

--- Consts.py
+++ Consts.py
@@ -1,1 +1,5 @@
-screen_size = (500,500)
+screen_size = (500,650)
+
+
+objects_list = [('fast_fashion','sustainable_fashion'),('meat','vegan'),('plastic_straw','reusable_cup'),
+                ('trash_bin','recycle_trash_bin')]
