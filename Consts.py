12d0a2b8ca4d49976ffa2033212368b1e93a0b38
pink-potao <lihi.zachis1223@gmail.com>
2022-09-21 11:40:12 +0300

    screen

--- Consts.py
+++ Consts.py
@@ -3,3 +3,5 @@ background_color = (255,255,255)
 objects_size = (4,4)
 objects_list = [('fast_fashion','sustainable_fashion'),('meat','vegan'),('plastic_straw','reusable_cup'),
                 ('trash_bin','recycle_trash_bin')]
+right_object_x = 240
+left_object_x = 260
--- Objects.py
+++ Objects.py
@@ -1,6 +1,15 @@
 import pygame
+import Consts
+import Screen
+
 
 def place_objects(cords_tuple, png_couple):
-    unsustainable_img = pygame.image.load("png files/injury.png")
-    unsustainable_img = pygame.transform.scale(unsustainable_img, (Consts.SIZE * 2, Consts.SIZE * 4))
+    unsustainable_object = png_couple[0]
+    unsustainable_img = pygame.image.load("png files/"+unsustainable_object+".png")
+    unsustainable_img = pygame.transform.scale(unsustainable_img, Consts.objects_size)
     Screen.screen.blit(unsustainable_img, cords_tuple)
+
+    sustainable_object = png_couple[1]
+    sustainable_img = pygame.image.load("png files/" + sustainable_object + ".png")
+    sustainable_img = pygame.transform.scale(sustainable_img, Consts.objects_size)
+    Screen.screen.blit(sustainable_img, cords_tuple)
