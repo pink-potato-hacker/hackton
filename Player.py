f8c0321ea564feeb92ed4c1184487a763fc952d9
Shoham <shoham1804@gmail.com>
2022-09-21 15:28:59 +0300

    match

--- Player.py
+++ Player.py
@@ -12,13 +12,22 @@ def player_image():
     return player_image
 
 player = pygame.Rect(100,550, Consts.PLAYER_WIDTH, Consts.PLAYER_HEIGHT)
+
+
 def left_key():
    if player.x > 100:
         player.x -= Consts.VEL
+
+
 def right_key():
     if player.x <  300:
         player.x += Consts.VEL
 
 
-def meeting_check(object_Y):
-  pass
+def meeting_check(object_Y, object):
+  if object_Y + 100 == 590:
+      if object in Consts.sustainable_list:
+          Consts.good_points += 1
+      else:
+          Consts.bad_points -= 1
+  return Consts.good_points, Consts.bad_points
